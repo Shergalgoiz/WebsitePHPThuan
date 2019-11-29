0b322253aa98d8496ea6bf23029e3526b7137b5f
Shergalgoiz <Shamanspid1996@gmail.com>
2019-11-29 22:34:41 +0700

    chinh sua te le chua tach ra file sua

--- admincp/modules/quanlyloaisp/lietke.php
+++ admincp/modules/quanlyloaisp/lietke.php
@@ -1,20 +1,34 @@
-<?php 
-    $conn = mysqli_connect("localhost","root", "", "webphpthuan");
-    $sql="SELECT * FROM loaisp ORDER BY id_loaisp desc";
-    $run = mysqli_query($conn, $sql);
+<?php
+$conn = mysqli_connect("localhost", "root", "", "webphpthuan");
+$sql = "SELECT * FROM loaisp ORDER BY id_loaisp desc";
+$run = mysqli_query($conn, $sql);
 ?>
 <table width="100%" border="1px">
 
-    <?php 
-        if ($run){
-            while($dong=mysqli_fetch_row($run)) {
-                printf ("ID_loaisp: %s  |   Tên Loại SP: %s  | Thứ Tự: %s 
-                </br>",$dong[0],$dong[1], $dong[2]);
-            }    
-        }
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Tên SP</th>
+            <th>TT</th>
+            <th>Thao tác</th>
+        </tr>
+    </thead>
+
+    <?php
+    if ($run) {
+        while ($item = mysqli_fetch_row($run)):
+            ?>
+            <tr>
+                <td><?php echo $item[0]; ?></td>
+                <td><?php echo $item[1]; ?></td>
+                <td><?php echo $item[2]; ?></td>
+                <td>
+                    <a href="modules/quanlyloaisp/xuly.php?action=edit&id=<?php echo $item[0] ?>">Sửa</a>
+                    <a href="modules/quanlyloaisp/xuly.php?action=delete&id=<?php echo $item[0] ?>">Xóa</a>
+                </td>
+            </tr>
+    <?php
+        endwhile;
+    }
     ?>
-    <tr>
-        <td>Sửa</td>
-        <td><a href="modules/quanlyloaisp/xuly.php?id=<?php echo $dong['id_loaisp'] ?>">Xóa</a></td>
-    </tr>
 </table>
--- admincp/modules/quanlyloaisp/xuly.php
+++ admincp/modules/quanlyloaisp/xuly.php
@@ -1,19 +1,65 @@
 <?php 
     include('../config.php');
-    $id=$_GET['id'];
-    $tenloaisanpham=$_POST['tenloaisanpham'];
-    $thutu=$_POST['thutu'];
+    $id=filter_input(INPUT_GET, 'id');
+    $action=$_GET['action'];
+ 
     if(isset($_POST['them'])) {
+        $tenloaisanpham=$_POST['tenloaisanpham'];
+        $thutu=$_POST['thutu'];
+        
         //them
         $sql="INSERT INTO loaisp (tenloaisanpham,thutu) VALUES ('$tenloaisanpham','$thutu')";
         mysqli_query($conn,$sql);
-        header('location:../../admincp.php?quanly=quanlyloaisp&ac=them');
-    } else if (isset($_POST['sua'])) {
-        //sua
-    } else {
+        header('location:../../admincp.php?quanly=quanlyloaisp&action=them');
+    } else if ($action == 'edit') {
+        
+        if (isset($_POST['submit'])) {
+            $tenloaisanpham=$_POST['tenloaisanpham'];
+            $thutu=$_POST['thutu'];
+            $sql="UPDATE loaisp SET tenloaisanpham='$tenloaisanpham', thutu=$thutu WHERE id_loaisp=$id";
+            echo $sql;
+            mysqli_query($conn, $sql);
+        }
+        
+        $sql = "SELECT * FROM loaisp WHERE id_loaisp=$id";
+        $run = mysqli_query($conn, $sql);
+        $item= mysqli_fetch_array($run);
+        print_r ($item);
+        ?>
+        
+        <form action="?id=<?php echo $id; ?>&action=edit" method="POST" enctype="mutilpart/form-data">
+            <table width="100%" border="1" align="center">
+                <tr>
+                    <td colspan="2">
+                        <div align="center">Sửa loại sản phẩm</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Tên sản phẩm</td>
+                    <td><input type="text" name="tenloaisanpham" value="<?php echo $item['tenloaisanpham'] ?>"></td>
+                </tr>
+                <tr>
+                    <td>thứ tự</td>
+                    <td><input type="text" name="thutu" value="<?php echo $item['thutu'] ?>"></td>
+                    <input name="submit" value="1" type="hidden">
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div align="center">
+                            <input type="submit" name="sua" id="sua" value="sửa">
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+
+        <?php
+
+    } else if ($action == 'delete' ){
         //xoa
         $sql="DELETE FROM loaisp WHERE id_loaisp='$id'";
         mysqli_query($conn,$sql);
-        header('location:../../admincp.php?quanly=quanlyloaisp&ac=them');
+        header('location:../../admincp.php?quanly=quanlyloaisp&action=them');
     }
 ?>
